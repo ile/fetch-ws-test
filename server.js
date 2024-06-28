78f30130f3d997b83e45e741770169d8b4a056d9
Ilkka Huotari <ilkkah@gmail.com>
2024-06-29 02:42:37 +0300

    ..

--- server.js
+++ server.js
@@ -12,7 +12,29 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const server = createServer(app);
-const wss = new WebSocketServer({ port: PORTWS, path: "/ws" });
+const wss = new WebSocketServer({
+  port: PORTWS,
+  path: "/ws",
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed if context takeover is disabled.
+  }
+});
 
 app.use(express.static(join(__dirname, 'public')));
 
